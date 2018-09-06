9914eaf27c979b9e013742751d7648ca9f4219a3
Egon Fiedler <efiedler93@gmail.com>
2018-09-06 14:55:27 -0700

    clean code, clean routes, better documentation

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 /////////////////////////////////////////
-//  This is the required variable to use express
+//  Required variable to use express
 /////////////////////////////////////////
 
 //This sets up the express framework enviroment
 var express = require('express');
-//It's so weird about Javascript that you can have a function in a variable
 var app = express();
-//This sets up handlebars
 var exphbs  = require('express-handlebars');
-
 var http = require('http');
-
 var giphy = require('giphy-api')();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -18,49 +14,25 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 /////////////////////////////////////////
-//  THIS ARE ROUTES
+//  ROUTES
 /////////////////////////////////////////
 
-//Route to get hello
-app.get('/hello-hello', function (req, res) {
-  res.send('Hey');
-});
-//Route that says hello
-app.get('/hello-world', function (req, res) {
-  res.send('Hello World');
-});
-//Route to show gif
-app.get('/hello-gif', function (req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
-  res.render('hello-gif', {gifUrl: gifUrl});
-});
-//Route to get names
-app.get('/greetings/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('greetings', {name: name});
-});
-//Route root 
-
+//GET root 
 app.get('/', function (req, res) {
-//   console.log('request=', req)
-//   giphy.search(req.query.term, function (err, response) {
-//     //console.log('response=', res)
-//     res.render('home', {gifs: response.data})
-//   });
-// });
   //console.log('request=', req)
   if (req.query.term != undefined &&  req.query.term != "") {
     giphy.search(req.query.term, function (err, response) {
-        console.log('response=', res)
-        res.render('home', {gifs: response.data})
-      });
+      //console.log('response=', res)
+      res.render('home', {gifs: response.data})
+    });
   }
-    else {
-        giphy.trending(function (err, response) {
-        res.render('home', {gifs: response.data})
-        });
-    }
+  else {
+    giphy.trending(function (err, response) {
+      res.render('home', {gifs: response.data})
+    });
+  }
 });
+
 /////////////////////////////////////////
 //  This connects with the server
 /////////////////////////////////////////
